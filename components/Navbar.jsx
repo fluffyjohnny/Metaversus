6be96c573bd61f1447840af25b9286fe981f97ed
John Chia <95445653+fluffyjohnny@users.noreply.github.com>
2022-12-07 13:22:28 -0800

    change viewport option to show up only once

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,20 @@
-'use client';
+"use client";
 
-import { motion } from 'framer-motion';
+import { motion } from "framer-motion";
 
-import styles from '../styles';
-import { navVariants } from '../utils/motion';
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
 
 const Navbar = () => (
   <motion.nav
     variants={navVariants}
     initial="hidden"
     whileInView="show"
+    viewport={{ once: true }}
     className={`${styles.xPaddings} py-8 relative`}
   >
     <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div
-      className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
-    >
+    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
       <img
         src="/search.svg"
         alt="search"
